baf651364fef4c1d1b0b4bb6543d0475295129bf
saahaaar <sahar.mansouri@esprit.tn>
2021-05-11 01:53:03 +0200

    7eme commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { CreerticketComponent } from './ticket/creerticket/creerticket.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component:AccueilComponent,children:[
+    {path: 'ticket', component:TicketComponent},
+    {path: 'creerticket', component:CreerticketComponent},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { CreerticketComponent } from './ticket/creerticket/creerticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    TicketComponent,
+    CreerticketComponent
   ],
   imports: [
     BrowserModule,
